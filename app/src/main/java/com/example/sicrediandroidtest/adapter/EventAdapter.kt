da3808590605e3f1a9eba78103a33d43897bbb90
Raul Cardoso <cavalcante.cardoso@gmail.com>
2022-07-16 23:44:18 -0300

    Fix load image http in Picasso lib; Insert shadow in cardview.

--- app/src/main/java/com/example/sicrediandroidtest/adapter/EventAdapter.kt
+++ app/src/main/java/com/example/sicrediandroidtest/adapter/EventAdapter.kt
@@ -1,19 +1,24 @@
 package com.example.sicrediandroidtest.adapter
 
+import android.content.Context
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.example.sicrediandroidtest.R
 import com.example.sicrediandroidtest.model.Event
+import com.google.android.material.card.MaterialCardView
 import com.squareup.picasso.Picasso
 
 class EventAdapter(
     private val listEvents: List<Event>,
-    private val onItemClick: (View, Event, Int) -> Unit
+    private val onItemClick: (View, Event, Int) -> Unit,
+    private val context: Context
 ): RecyclerView.Adapter<EventAdapter.EventViewHolder>() {
 
     override fun onCreateViewHolder(
@@ -22,7 +27,13 @@ class EventAdapter(
     ): EventViewHolder {
         val v = LayoutInflater.from(parent.context)
             .inflate(R.layout.item_event, parent, false)
+
         val vh = EventViewHolder(v)
+        val card: CardView = vh.cardEvent
+        card.setCardBackgroundColor(Color.parseColor("#E6E6E6"));
+        card.maxCardElevation = 0.0F;
+        card.radius = 5.0F;
+
         vh.layoutEvent.setOnClickListener { view ->
             val position = vh.absoluteAdapterPosition
             val event = listEvents[position]
@@ -34,8 +45,10 @@ class EventAdapter(
     override fun onBindViewHolder(holder: EventViewHolder, position: Int) {
         val event = listEvents[position]
         holder.run {
-            Picasso.get()
-                .load(event.image)
+            Picasso.with(context)
+                .load(event.image.replace("http://", "https://"))
+                .fit()
+                .centerCrop()
                 .into(imgEvent)
             txtTitleEvent?.text = event.title
         }
@@ -47,5 +60,6 @@ class EventAdapter(
         var imgEvent: ImageView? = view.findViewById(R.id.ivImageEvent)
         var txtTitleEvent: TextView? =  view.findViewById(R.id.tvTitleEvent)
         var layoutEvent: ConstraintLayout = view.findViewById(R.id.layoutEvent)
+        var cardEvent: MaterialCardView = view.findViewById(R.id.mcCardEvent)
     }
 }
