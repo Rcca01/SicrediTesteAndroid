b90135e3eb182d2c28e2f263c6eb8ede1ac9df06
Raul Cardoso <cavalcante.cardoso@gmail.com>
2022-07-16 23:45:17 -0300

    Manager loading.

--- app/src/main/java/com/example/sicrediandroidtest/MainActivity.kt
+++ app/src/main/java/com/example/sicrediandroidtest/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -16,22 +17,27 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var rvEvents: RecyclerView
     private lateinit var eventAdapter: EventAdapter
+    private lateinit var progressBar: ProgressBar
     private val viewModel: EventViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_events)
         rvEvents = findViewById(R.id.rvEvents)
+        progressBar = findViewById(R.id.progressBar)
+        progressBar.visibility = View.VISIBLE
         initRecycleView()
         viewModel.getListEvents(this)
+
     }
 
     private fun initRecycleView() {
         viewModel.listEvents().observe(this, Observer {
-            eventAdapter = EventAdapter(it, this::onItemClick)
+            eventAdapter = EventAdapter(it, this::onItemClick, this)
             rvEvents.adapter = eventAdapter
             val layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
             rvEvents.layoutManager = layoutManager
+            progressBar.visibility = View.GONE
         })
     }
 
